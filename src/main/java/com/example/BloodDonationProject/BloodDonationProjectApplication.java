7e4b2f9a5ec0e67418c31ace1be81e78fde28130
SalehaZainab <szainab5160@gmail.com>
2025-11-30 00:43:57 +0500

    Initial commit

---
+++ src/main/java/com/example/BloodDonationProject/BloodDonationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BloodDonationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BloodDonationProject/BloodDonationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BloodDonationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
